a8be53cf89ba31b7e8bc5650c9e4de051f8581f4
Honorable Con <34490054+honorableCon@users.noreply.github.com>
2020-06-29 19:36:30 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'notifications',
+    loadChildren: () => import('./pages/notifications/notifications.module').then(m => m.NotificationsPageModule)
+  },
+  {
+    path: 'statistiques',
+    loadChildren: () => import('./pages/statistiques/statistiques.module').then(m => m.StatistiquesPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+// app.component.ts
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+
+
+  currentPageTitle = 'Home';
+
+  appPages = [
+    {
+      title: 'Tracker',
+      url: '',
+      icon: 'location'
+    },
+    {
+      title: 'Notifications',
+      url: '/notifications',
+      icon: 'notifications'
+    },
+    {
+      title: 'Statistiques',
+      url: '/statistiques',
+      icon: 'stats-chart'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
